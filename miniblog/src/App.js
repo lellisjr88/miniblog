3abd087f2cf7a2c24602fe29980709041de026eb
Lellis Júnior <92763629+lellisjr88@users.noreply.github.com>
2024-08-29 15:29:15 +0000

    AutContext

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -1,6 +1,16 @@
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
 import './App.css';
+
+//Context
+
+import { AuthProvider } from "./context/AuthContext";
+import { onAuthStateChanged } from "firebase/auth";
+
+//hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
+
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Navbar from "./components/Navbar";
@@ -9,20 +19,37 @@ import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register";
 
 function App() {
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
+
+  const loadingUser = user == undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Caregando...</p>
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar/>
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home></Home>}></Route>
-            <Route path="/about" element={<About></About>}></Route>
-            <Route path="/login" element={<Login></Login>}></Route>
-             <Route path="/register" element={<Register></Register>}></Route>
-          </Routes>
-        </div>
-        <Footer/>
-      </BrowserRouter>
+      <AuthProvider value={{user}}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home></Home>}></Route>
+              <Route path="/about" element={<About></About>}></Route>
+              <Route path="/login" element={<Login></Login>}></Route>
+              <Route path="/register" element={<Register></Register>}></Route>
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
